936b3f2406b73bbc305f8f63ad7ce710bbdc4d94
Ridham Khandar <ridhamk.j@gmail.com>
2023-08-02 22:46:50 +0530

    Time: 2 ms (91.44%), Space: 40.7 MB (47.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1){
+            return false;
+        }
+        if(s.charAt(0)==']'||s.charAt(0)=='}'||s.charAt(0)==')'){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' ||s.charAt(i)=='{' ){
+                stack.push(s.charAt(i));
+            }
+            else if(s.charAt(i)==')' || s.charAt(i)==']' ||s.charAt(i)=='}' ) {
+                char ch=s.charAt(i);
+                if(stack.empty() && i<s.length()){
+                    return false;
+                }
+                if(stack.peek()=='(' && ch==')'){
+                    stack.pop();
+                }
+                else if(stack.peek()=='{' && ch=='}'){
+                    stack.pop();
+                }
+                else if(stack.peek()=='[' && ch==']'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+
+        if(stack.empty())
+            return true;
+        return false;
+    }
+}
