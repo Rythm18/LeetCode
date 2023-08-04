8300cd2f5b5ae9f31eebc2999ed689ac20b05162
Ridham Khandar <ridhamk.j@gmail.com>
2023-08-04 08:38:05 +0530

    Time: 9 ms (34.7%), Space: 57.2 MB (87.17%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+                int correct=nums[i]-1;
+                if(nums[i]>0&& nums[i]<=nums.length  && nums[i]!=nums[correct]  )
+                {
+                    swap(nums,i,correct);
+                }
+                else{
+                    i++;
+                }
+            
+        }
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=j+1){
+                return j+1;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return nums.length+1;
+    }
+    public void swap(int[] nums,int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+}
