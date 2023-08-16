1ee8f493b399643e2d1022509589bb78539f7282
Ridham Khandar <ridhamk.j@gmail.com>
2023-08-16 22:25:58 +0530

    Time: 41 ms (95.48%), Space: 92.4 MB (38.71%) - LeetHub

--- 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
@@ -4,12 +4,15 @@ class Solution {
         long count=0;
         for(int i=0;i<rec.length;i++)
         {
-            if(map.containsKey((double)rec[i][0]/rec[i][1]))
-                count+=map.get((double)rec[i][0]/rec[i][1]);
+            // if(map.containsKey((double)rec[i][0]/rec[i][1]))
+            //     count+=map.get((double)rec[i][0]/rec[i][1]);
             map.put((double)rec[i][0]/rec[i][1],map.getOrDefault((double)rec[i][0]/rec[i][1],0L)+1);
+        }
+
+        for(long val: map.values()){
+            count+=(val*(val-1))/2;
         }
         
         return count;
-        
     }
 }
