30d5d2f2d6b7c15d1d5d94ad5e37f3f0dc31e6fc
Ridham Khandar <ridhamk.j@gmail.com>
2023-09-01 17:49:55 +0530

    Time: 2 ms (73.73%), Space: 46.6 MB (37.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -4,20 +4,10 @@ class Solution {
 
         ans[0]=0;
         for(int i=1;i<n+1;i++){
-            int x=countB(i);
-            ans[i]=x;
+            
+            ans[i]=ans[i/2]+i%2;
         }
         return ans;
     }
-    public int countB(int n)
-    {
-        int ans=0;
-        String str=Integer.toBinaryString(n);
-        for(char s : str.toCharArray())
-        {
-            if(s=='1')
-                ans++;
-        }
-        return ans;
-    }
+   
 }
